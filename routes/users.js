f5848cb794449fee9f82bf3dbaaffa71aa0720ae
Irsan Sebastian <irsansebastianc4@gmail.com>
2017-03-17 15:28:34 +0700

    route user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const controllers = require('../controllers/food')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
+router.post('/food', controllers.create);
 
 module.exports = router;
