b0f923c6aeefbd276477de04d23aea021d1ff2d7
Irsan Sebastian <irsansebastianc4@gmail.com>
2017-03-17 15:29:06 +0700

    controller + testing

---
+++ controllers/food.js
@@ -0,0 +1,33 @@
+const model = require('../models/food')
+
+module.exports = {
+
+
+    create : function(req,res){
+
+        let tags = req.body.food_tags.split(" ")
+
+        let food = {
+          _userId: req.body._userid,
+          food_title: req.body.food_title,
+          food_pic : req.body.food_pic,
+          food_price: req.body.food_price,
+          food_qty:  req.body.food_qty,
+          food_tags :tags,
+          food_desc : req.body.food_desc,
+          status : 1
+        }
+
+        model.create(food)
+        .then(function(data){
+          if(data) res.json({success : data})
+        })
+        .catch(function(err){
+          if(err) res.json({err : err})
+        })
+    }
+
+
+
+
+}
---
+++ test/test.js
@@ -0,0 +1,35 @@
+const chai = require('chai')
+const expect = require('chai').expect
+
+const chaiHttp = require('chai-http')
+chai.use(chaiHttp)
+
+const app = 'http://localhost:3000'
+
+describe('Testing CRUD - food', function () {
+
+  it('result - Post a food', function (done) {
+    chai.request(app)
+      .post('/users/food')
+      .send({
+        _userId: "someuserobject1d",
+        food_title: "Nasi Bebek Sambel Ijo",
+        food_pic : "nasibebek.jpg",
+        food_price: "35000",
+        food_qty  : 3,
+        food_tags :"pedas enak gurih",
+        food_desc : "deskripsi tentang sebuah makanan",
+        _requestId : "somerequestid",
+        status : 1
+      })
+      .end(function (err, res) {
+
+        expect(res).to.have.status(200);
+        expect(res).to.be.an('object');
+        done()
+      })
+  })
+
+
+
+  })
