bd3da18339cfdc52aa32ddcbaef00311da252f0f
Irsan Sebastian <irsansebastianc4@gmail.com>
2017-03-20 15:54:44 +0700

    route user

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,16 @@ const controllers = require('../controllers/food')
 const requestController = require('../controllers/request')
 
 /* GET users listing. */
-router.get('/food', controllers.read);
-router.post('/food', controllers.create);
+router.get('/food', controllers.read)
+router.post('/food', controllers.create)
 router.put('/food', controllers.update)
+router.delete('/food', controllers.delete)
 
-router.post('/request', requestController.create);
-router.get('/request', requestController.read);
-router.put('/request', requestController.update);
+router.post('/request', requestController.create)
+router.get('/request', requestController.read)
+router.put('/request', requestController.update)
+router.delete('/request',requestController.delete)
+
 
 
 module.exports = router;
