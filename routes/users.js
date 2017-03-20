2832cb5ec35e299b17a07be5907c29555bb53d65
Muhammad Iqbal <iqbal.0809@gmail.com>
2017-03-20 16:46:57 +0700

    router user IQBAL

--- routes/users.js
+++ routes/users.js
@@ -2,17 +2,33 @@ var express = require('express');
 var router = express.Router();
 const controllers = require('../controllers/food')
 const requestController = require('../controllers/request')
+const userController = require('../controllers/user');
 const multer = require('multer');
-const path = require('path');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/images/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, 'botram' + '-' + Date.now() + '.' + file.mimetype.split('/')[1])
+  }
+})
 
 /* GET users listing. */
 router.get('/food', controllers.read);
-router.post('/food', multer({ dest: './imageMulter/' }).single('picture'), controllers.create);
+router.post('/food', multer({ storage: storage }).single('picture'), controllers.create);
 router.put('/food', controllers.update)
 
 router.post('/request', requestController.create);
 router.get('/request', requestController.read);
 router.put('/request', requestController.update);
+
+router.get('/', userController.list);
+router.get('/:id', userController.show);
+router.post('/', multer({ storage: storage }).single('picture'), userController.create);
+router.put('/:id', userController.update);
+router.put('/:id/favbysearch', userController.favBySearch);
+router.put('/:id/addrating', userController.addRating);
+router.delete('/:id', userController.remove);
 
 
 module.exports = router;
