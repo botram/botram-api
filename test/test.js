a7bc56d57df1ddc01dbd3ec6a7dd2e2cd9b630b6
Irsan Sebastian <irsansebastianc4@gmail.com>
2017-03-21 19:30:48 +0700

    test.js

--- test/test.js
+++ test/test.js
@@ -8,26 +8,26 @@ const app = 'http://localhost:3000'
 
 describe('Testing CRUD - food', function () {
 
-  it('result - Post a food', function (done) {
-    chai.request(app)
-      .post('/users/food')
-      .send({
-        food_title: "Nasi Bebek Sambel Ijo",
-        food_price: "35000",
-        food_qty  : 3,
-        food_pic : "food.jpg",
-        food_tags :"pedas enak gurih",
-        food_desc : "deskripsi tentang sebuah makanan",
-        status : 1
-      })
-      .end(function (err, res) {
-  
-        expect(res).to.have.status(200);
-        expect(res).to.be.an('object');
-
-        done()
-      })
-  })
+  // it('result - Post a food', function (done) {
+  //   chai.request(app)
+  //     .post('/users/food')
+  //     .send({
+  //       food_title: "Nasi Bebek Sambel Ijo",
+  //       food_price: "35000",
+  //       food_qty  : 3,
+  //       food_pic : "food.jpg",
+  //       food_tags :"pedas enak gurih",
+  //       food_desc : "deskripsi tentang sebuah makanan",
+  //       status : 1
+  //     })
+  //     .end(function (err, res) {
+  //
+  //       expect(res).to.have.status(200);
+  //       expect(res).to.be.an('object');
+  //
+  //       done()
+  //     })
+  // })
 
   it('result - Post a food, but food_pic is empty', function (done) {
     chai.request(app)
@@ -96,9 +96,6 @@ describe('Testing CRUD - food', function () {
       })
   })
 
-
-
-
   it('result - Get all food', function (done) {
     chai.request(app)
 
@@ -149,9 +146,9 @@ it('result - Post a request', function (done) {
   chai.request(app)
     .post('/users/request')
     .send({
-      _foodId : "58cbb54a7171b1237c5dd81f",
-      request_notes :"testing request something",
-      request_qty :2,
+      _foodId : "58d0d986362503157eb7a4fc",
+      request_notes :"Irsan testing request again !",
+      request_qty :1,
       status : 0
     })
     .end(function (err, res) {
@@ -161,6 +158,66 @@ it('result - Post a request', function (done) {
       done()
     })
 })
+
+it('result - Post a request, request_qty diisi dengan String', function (done) {
+  chai.request(app)
+    .post('/users/request')
+    .send({
+      _foodId : "58d0d986362503157eb7a4fc",
+      request_notes :"testing request something",
+      request_qty :"somestring",
+      status : 0
+    })
+    .end(function (err, res) {
+
+      expect(res).to.have.status(200);
+      expect(res).to.be.an('object');
+      expect(res.body.err).to.be.an('object');
+      expect(res.body.err.message).to.equal('Request validation failed')
+      done()
+
+    })
+})
+
+it('result - Post a request, request_qty tidak diisi', function (done) {
+  chai.request(app)
+    .post('/users/request')
+    .send({
+      _foodId : "58d0d986362503157eb7a4fc",
+      request_notes :"testing request something",
+      request_qty :'',
+      status : 0
+    })
+    .end(function (err, res) {
+
+      expect(res).to.have.status(200);
+      expect(res).to.be.an('object');
+      expect(res.body.err).to.be.an('object');
+      expect(res.body.err.message).to.equal('Request validation failed')
+      done()
+    })
+})
+
+// it('result - put a request, seller menerima request dari user [syaratnya, harus post request baru untuk sebuah makanan, lalu ambil request idny, dan put disini]', function (done) {
+//   chai.request(app)
+//     .put('/users/request')
+//     .send({
+//       _requestId : "",
+//     })
+//     .end(function (err, res) {
+//
+//
+//       expect(res).to.have.status(200);
+//       expect(res).to.be.an('object');
+//       expect(res.body).to.have.property("request")
+//       expect(res.body).to.have.property("food")
+//       expect(res.body.request.status).to.equal(1)
+//
+//       done()
+//     })
+// })
+
+
 
 it('result - Read all request', function (done) {
   chai.request(app)
@@ -172,5 +229,46 @@ it('result - Read all request', function (done) {
       done()
     })
 })
+
+})
+
+describe('Testing CRUD - User', function () {
+
+  it('result - Create a user', function (done) {
+    let dummy = {
+      name : "budi",
+        email : "budi@gmail.com",
+        pic : "budi.png"
+    }
+    chai.request(app)
+      .post('/users')
+      .send({
+        name : "budi",
+    			email : "budi@gmail.com",
+          pic : "budi.png"
+      })
+      .end(function (err, res) {
+
+        expect(res).to.have.status(200);
+        expect(res).to.be.an('object');
+        expect(res.body).to.have.property("name")
+        expect(res.body).to.have.property("email")
+        expect(res.body).to.have.property("pic")
+        done()
+      })
+  })
+
+it('result - Get all user', function (done) {
+  chai.request(app)
+    .get('/users')
+    .end(function (err, res) {
+      expect(res).to.have.status(200);
+      expect(res).to.be.an('object');
+      expect(res.body).to.have.length.above(0)
+      done()
+    })
+})
+
+
 
 })
