2f77c84cf40f5660cdc8f7949a89130485a9e79f
Irsan Sebastian <irsansebastianc4@gmail.com>
2017-03-21 12:00:48 +0700

    testing sementara tanpa upload file

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 const chai = require('chai')
 const expect = require('chai').expect
-
+const Request = require('supertest')
 const chaiHttp = require('chai-http')
 chai.use(chaiHttp)
 
@@ -11,11 +11,32 @@ describe('Testing CRUD - food', function () {
   it('result - Post a food', function (done) {
     chai.request(app)
       .post('/users/food')
-      .attach('food_pic', './test/test_image/botram.png')
       .send({
         food_title: "Nasi Bebek Sambel Ijo",
         food_price: "35000",
         food_qty  : 3,
+        food_pic : "food.jpg",
+        food_tags :"pedas enak gurih",
+        food_desc : "deskripsi tentang sebuah makanan",
+        status : 1
+      })
+      .end(function (err, res) {
+  
+        expect(res).to.have.status(200);
+        expect(res).to.be.an('object');
+
+        done()
+      })
+  })
+
+  it('result - Post a food, but food_pic is empty', function (done) {
+    chai.request(app)
+      .post('/users/food')
+      .send({
+        food_title: "Nasi Bebek Sambel Ijo",
+        food_price: "35000",
+        food_qty  : 3,
+        food_pic : "",
         food_tags :"pedas enak gurih",
         food_desc : "deskripsi tentang sebuah makanan",
         status : 1
@@ -24,9 +45,59 @@ describe('Testing CRUD - food', function () {
 
         expect(res).to.have.status(200);
         expect(res).to.be.an('object');
+        expect(res.body.err).to.be.an('object');
+        expect(res.body.err.message).to.equal('Food validation failed')
+
         done()
       })
   })
+
+  it('result - Post a food, but food_title is empty', function (done) {
+    chai.request(app)
+      .post('/users/food')
+      .send({
+        food_title: "",
+        food_price: "35000",
+        food_qty  : 3,
+        food_pic : "food_pic.jpg",
+        food_tags :"pedas enak gurih",
+        food_desc : "deskripsi tentang sebuah makanan",
+        status : 1
+      })
+      .end(function (err, res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.an('object');
+        expect(res.body.err).to.be.an('object');
+        expect(res.body.err.message).to.equal('Food validation failed')
+
+        done()
+      })
+  })
+
+  it('result - Post a food, but food_price is string', function (done) {
+    chai.request(app)
+      .post('/users/food')
+      .send({
+        food_title: "food title",
+        food_price: "some String",
+        food_qty  : 3,
+        food_pic : "food_pic.jpg",
+        food_tags :"pedas enak gurih",
+        food_desc : "deskripsi tentang sebuah makanan",
+        status : 1
+      })
+      .end(function (err, res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.an('object');
+        expect(res.body.err).to.be.an('object');
+        expect(res.body.err.message).to.equal('Food validation failed')
+
+        done()
+      })
+  })
+
+
+
 
   it('result - Get all food', function (done) {
     chai.request(app)
@@ -36,6 +107,8 @@ describe('Testing CRUD - food', function () {
 
         expect(res).to.have.status(200);
         expect(res).to.be.an('object');
+        expect(res.body).to.have.property('success')
+        expect(res.body.success).to.have.length.above(0)
         done()
       })
   })
@@ -47,6 +120,21 @@ describe('Testing CRUD - food', function () {
       .end(function (err, res) {
         expect(res).to.have.status(200);
         expect(res).to.be.an('object');
+        expect(res.body).to.have.property('success')
+        expect(res.body.success).to.have.length.above(0)
+        done()
+      })
+  })
+
+  it('result - Searching food but not found', function (done) {
+    chai.request(app)
+
+      .get('/users/food/something')
+      .end(function (err, res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.an('object');
+        expect(res.body).to.have.property('success')
+        expect(res.body.success).to.have.lengthOf(0)
         done()
       })
   })
